1fb9b4641fb734e144b3b06453fc303293fbbdce
Sage Wall <sagewall@users.noreply.github.com>
2020-02-10 15:46:10 -0700

    Add basic directions between two locations

--- src/app/directions-map/directions-map.component.ts
+++ src/app/directions-map/directions-map.component.ts
@@ -8,10 +8,15 @@ import { } from 'googlemaps';
 })
 export class DirectionsMapComponent implements OnInit {
 
+  directionsService: google.maps.DirectionsService;
+  directionsRenderer: google.maps.DirectionsRenderer;
   map: google.maps.Map;
 
   @ViewChild('mapNode', { static: true })
   mapNode: ElementRef;
+
+  @ViewChild('directionsNode', { static: true })
+  directionsNode: ElementRef;
 
   constructor() { }
 
@@ -20,6 +25,9 @@ export class DirectionsMapComponent implements OnInit {
   }
 
   initMap() {
+    this.directionsService = new google.maps.DirectionsService();
+    this.directionsRenderer = new google.maps.DirectionsRenderer();
+
     const mapOptions = {
       center: new google.maps.LatLng(39.736, -105.139),
       zoom: 15,
@@ -27,5 +35,23 @@ export class DirectionsMapComponent implements OnInit {
     };
 
     this.map = new google.maps.Map(this.mapNode.nativeElement, mapOptions);
+    this.directionsRenderer.setMap(this.map);
+    this.directionsRenderer.setPanel(this.directionsNode.nativeElement);
+
+    this.calcRoute();
+  }
+
+  calcRoute() {
+    const directionsOptions: google.maps.DirectionsRequest = {
+      origin: '12553 W 12th Pl, Golden, Colorado, 80401',
+      destination: '3500 Illinois St, Golden, Colorado, 80401',
+      travelMode: google.maps.TravelMode.DRIVING
+    };
+
+    this.directionsService.route(directionsOptions, (result, status) => {
+      if (status === 'OK') {
+        this.directionsRenderer.setDirections(result);
+      }
+    });
   }
 }
